f6849be84b5261ac96e18b49182b0ee58c0c294e
Modupe <laosunmodupe@gmail.com>
2024-02-11 21:25:42 +0100

    my third commit

---
+++ index.js
@@ -0,0 +1,52 @@
+// let firstHouseNumber =30
+
+// let secondHouseNumber =40
+
+// let ThirdHouseNumber =20
+
+// let guessedHouseNumber =prompt(”Guess house number”)
+
+// guessedHousednumber = Number(gussedHouseNumber)
+
+// switch(true){
+
+// case guessedHouseNumber === firstHouseNumber :
+
+// console.log(`you have guessed the first number correctly` )
+
+// break
+
+// case guessedHouseNumber === secondHouseNumber :
+
+// console.log(`you have guessed the second number correctly` )
+
+// break
+
+// case guessedHouseNumber === thirdHouseNumber :
+
+// console.log(`you have guessed the third number correctly` )
+
+// break
+
+// default:
+
+// console.log(`you did not guess any house number correctly` )
+
+// }
+
+// task for javascript assessment
+
+
+let voteAge = 18
+
+
+
+// loop task
+let sum = 1;
+
+    for (let i = 0; i <= 6; i++) {
+        let sum =1
+        console.log(i)
+    }
+
+    return sum;
